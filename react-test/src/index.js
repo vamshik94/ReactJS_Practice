625b4229467c4f0335529a3b587bf4403b13a0a5
Venkata Vamshi Krishna Katragadda <vamshikrishna0694@gmail.com>
2017-08-25 15:23:04 -0500

    added import packages in index.js

--- react-test/src/index.js
+++ react-test/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/css/bootstrap-theme.css';
 ReactDOM.render(
   <App />,
   document.getElementById('root')
